c64524d04866aec1af4a9333bcf2337a94ccb18f
Fabien Walesch <fabien.walesch@gmail.com>
2018-08-20 13:01:50 +0200

    commit 13h01//20/08/2018

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 console.log(`Welcome to this guessing game!`);
 
 // This line randomly generates a number between 1 and 100
-const result = Math.floor(Math.random() * 100) + 1;
-console.log(`The solution is ${result}`);
+const soluce = Math.floor(Math.random() * 100) + 1;
+console.log(`The solution is ${soluce}`);
 
 // Add code 
 
+
+// DOM interaction
 window.addEventListener('DOMContentLoaded', function() {
     // The DOM is ready, we can handle it
     let intro = window.document.querySelector('#intro');
     intro.innerHTML = `Welcome to this guessing game!`;
-    let showResult = window.document.querySelector('#result');
-    showResult.innerHTML = `The solution is ${result}`;
+    let showSoluce = window.document.querySelector('#soluce');
+    showSoluce.innerHTML = `The solution is ${soluce}`;
 });
 
 
