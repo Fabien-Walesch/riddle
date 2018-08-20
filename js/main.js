7daaae814a3c5a13e664286e3b11d3f28ff9b3ea
Fabien Walesch <fabien.walesch@gmail.com>
2018-08-20 11:20:43 +0200

    commit 11h20//20/08/2018

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log(`Welcome to this guessing game!`);
+
+// This line randomly generates a number between 1 and 100
+const result = Math.floor(Math.random() * 100) + 1;
+console.log(`The solution is ${result}`);
+
+// Add code 
+
+
+
