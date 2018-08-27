62fa2dfc20c96239dc132c2a3d681d46b50b19fe
Fabien Walesch <fabien.walesch@gmail.com>
2018-08-27 14:52:10 +0200

    commit 14h51//27/08/2018

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ console.log(`The solution is ${soluce}`);
 
 // Add code 
 
-function getNumber() 
+/*function getNumber() 
 { 
   let number = document.getElementById("number").value; 
   console.log(number);
-}
+}*/
 
-/*let number = Number(prompt(`Entrez un nombre :`));
+let number = Number(prompt(`Entrez un nombre :`));
 let attempt = 0;
  
 while((number !== soluce) && (attempt < 6)){
@@ -30,7 +30,7 @@ if(number == soluce) {
     console.log(`Bravo ! La solution est ${soluce}`);
 } else {
     console.log(`Vous avez perdu!`);
-}*/
+}
 
 // DOM interaction
 window.addEventListener('DOMContentLoaded', function() {
